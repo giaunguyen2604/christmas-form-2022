3a02f457c4c6aa78d0f5ef6512935366726fa6e5
giaunguyen2604 <giau.nguyen@executionlab.asia>
2022-12-12 14:00:43 +0700

    build form input name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { useEffect, useState } from "react";
+import "./App.css";
+import Swal from "sweetalert2";
+import Snowfall from "react-snowfall";
+import Loading from "./Loading";
+
+const apiURL = "https://api.apispreadsheets.com/data/NgI8dqq84Jv0OAYp/";
+const shortToast = Swal.mixin({
+  toast: true,
+  position: "top-right",
+  timer: "2000",
+  showConfirmButton: false,
+});
+
+const randomCardURL = () => {
+  const randomNum = Math.floor(Math.random() * 100 + 1);
+  if (randomNum % 2 === 0) {
+    return "/card1.png";
+  } else {
+    return "/card2.png";
+  }
+};
+
+const SUBMIT_STATUS = {
+  none: 0,
+  pending: 1,
+  success: 2,
+};
 
 function App() {
+  const [name, setName] = useState("");
+  const [submitStatus, setSubmitStatus] = useState(SUBMIT_STATUS.none);
+  const [lcStatus, setlcStatus] = useState(SUBMIT_STATUS.none);
+
+  const [url] = useState(randomCardURL());
+
+  useEffect(() => {
+    const lcSmTt = localStorage.getItem("sm_tt");
+    if (lcSmTt) {
+      setlcStatus(parseInt(lcSmTt));
+    } else {
+      setlcStatus(SUBMIT_STATUS.none);
+    }
+  }, []);
+
+  const successAction = () => {
+    localStorage.setItem("sm_tt", 2);
+    setSubmitStatus(SUBMIT_STATUS.success);
+    setlcStatus(SUBMIT_STATUS.success);
+  };
+
+  const failAction = () => {
+    localStorage.setItem("sm_tt", 0);
+    setSubmitStatus(SUBMIT_STATUS.none);
+    setlcStatus(SUBMIT_STATUS.none);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitStatus(SUBMIT_STATUS.pending);
+    const data = { Name: name, Created_at: new Date().toLocaleString() };
+    fetch(apiURL, {
+      method: "POST",
+      body: JSON.stringify({ data }),
+    }).then((res) => {
+      if (res.status === 201) {
+        successAction();
+        shortToast.fire(
+          "Success! Your submission has been saved! ",
+          "",
+          "success"
+        );
+      } else {
+        failAction();
+        shortToast.fire("An unexpected error occurred", "", "error");
+      }
+    });
+  };
+
+  const isDisabled = submitStatus !== SUBMIT_STATUS.none;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <img src="/logo-exe.svg" alt="logo executionlab" className="logo-te" />
+      <Snowfall />
+      {lcStatus === SUBMIT_STATUS.none ? (
+        <div className="app-form">
+          <form onSubmit={handleSubmit}>
+            <div className="text-field">
+              <label htmlFor="name">Your name</label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Enter your name"
+                autoComplete="off"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit" className="button" disabled={isDisabled}>
+              Submit
+            </button>
+            {submitStatus === SUBMIT_STATUS.pending && (
+              <div className="loading">
+                <Loading />
+              </div>
+            )}
+          </form>
+        </div>
+      ) : (
+        <div className="submitted">
+          <h2>Your submission has been saved!</h2>
+          <button className="button" onClick={failAction}>
+            Resubmit
+          </button>
+        </div>
+      )}
+      <div className="christmas-card-wrapper">
+        <img srcset={`${url} 3x`} alt="" className="christmas-card" />
+      </div>
     </div>
   );
 }
---
+++ src/Loading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div class="loader">
+      <div class="inner one"></div>
+      <div class="inner two"></div>
+      <div class="inner three"></div>
+    </div>
+  );
+};
+
+export default Loading;
